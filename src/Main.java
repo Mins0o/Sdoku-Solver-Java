14ea9106036faf800dc856c3895cf9ce1dfb7606
MINSOO KANG <stengine2@gmail.com>
2020-02-25 14:05:06 +0900

    Git organized
    Frankly, this project is abandoned. But I think the algorithm still works.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] Args) throws IOException {
+		short[] list = {5, 3, 0, 0, 7, 0, 0, 0, 0, 6, 0, 0, 1, 9, 5, 0, 0, 0, 0, 9, 8, 0, 0 ,0 ,0, 6, 0, 8, 0, 0, 0, 6, 0, 0, 0, 3, 4, 0, 0, 8, 0, 3, 0, 0, 1 ,7, 0, 0 ,0 ,2 ,0, 0, 0 ,6, 0, 6 ,0, 0, 0, 0, 2, 8, 0, 0, 0, 0, 4, 1, 9, 0, 0 ,5 ,0 ,0, 0, 0, 8, 0, 0, 7, 9};
+		Sudoku_Puzzle first= new Sudoku_Puzzle(list);
+		first.solve();first.solve();
+		first.print();
+		System.in.read();
+ 	}
+	
+	public void formatter(short[] list) {
+		
+	}
+}
